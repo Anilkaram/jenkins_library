4ca042264e47545083fbc86d8d5c2085aa38ebfe
Anilkumar Karampuri <96483744+Anilkaram@users.noreply.github.com>
2025-07-31 12:01:31 +0530

    Update dockerPipeline.groovy

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -1,4 +1,4 @@
-/**def call(Map config = [:]) {
+def call(Map config = [:]) {
     def helper = new org.example.VaultDockerHelper(this)
     
     pipeline {
@@ -26,28 +26,5 @@
                 }
             }
         }
-    }
-}
-*/
-def call(String vaultPath, List secretMapping, Map config = [:], Closure body) {
-    def vaultConfig = [
-        disableChildPoliciesOverride: false,
-        engineVersion: 2,
-        timeout: 60,
-        vaultCredentialId: 'vault_token',
-        vaultUrl: 'http://127.0.0.1:8200',
-        prefixPath: 'secret'
-    ] + config // allow overrides
-
-    def vaultSecrets = [[
-        path: vaultPath,
-        secretValues: secretMapping
-    ]]
-
-    withVault(
-        configuration: vaultConfig,
-        vaultSecrets: vaultSecrets
-    ) {
-        body()
     }
 }
